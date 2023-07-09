ee7103b75ec5225c1c4b7329bfdf4e09b97378af
Nabeel <nabeel.sddq@gmail.com>
2023-07-08 20:30:32 -0400

    test

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -137,6 +137,8 @@ class UserService {
     public async getUserData(id: any): Promise<any> {
         console.log("pee")
         console.log(typeof (id));
+        const num = id;
+        console.log(num)
         const [
             UserData,
             WeightData,
@@ -154,12 +156,12 @@ class UserService {
                 any,
             ]
             = [
-                await this.db('users').where({ id: id }),
-                await this.db('weights').where({ id: id }).orderBy("record_date", "desc").limit(7),
-                await this.db('daily_activities').where({ id: id }),
-                await this.db('weekly_summary').where({ id: id }),
-                await this.db('daily_food').where({ id: id }),
-                await this.db('daily_workout').where({ id: id })
+                await this.db('users').where({ id: num }),
+                await this.db('weights').where({ id: num }).orderBy("record_date", "desc").limit(7),
+                await this.db('daily_activities').where({ id: num }),
+                await this.db('weekly_summary').where({ id: num }),
+                await this.db('daily_food').where({ id: num }),
+                await this.db('daily_workout').where({ id: num })
             ];
         return { UserData, WeightData, DailyActivities, WeeklySummary, DailyFood, DailyWorkout };
     }
